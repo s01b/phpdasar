94dc82738b204a1e8cbf58320a8ac5e83b40adac
Sokhib Sultoni <ssooiibb@gmail.com>
2019-08-14 04:38:00 +0700

    belajar tentang insert & delete di PHP

---
+++ pertemuan10/tambah.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tambah data mahasiswa</title>
+</head>
+
+<body>
+    <h1>Tambah data mahasiswa</h1>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="nrp">NRP : </label>
+                <input type="text" name="nrp" id="nrp">
+            </li>
+            <li>
+                <label for="username">Username : </label>
+                <input type="text" name="username" id="username">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambahkan Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+
+</html>
